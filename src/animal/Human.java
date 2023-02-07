3873d7fa9d9625b41e24271132558a12a058aa4e
syaketaro <a.tomm722@icloud.com>
2023-02-07 15:35:16 +0900

    kadai-java-review04

---
+++ src/animal/Human.java
@@ -0,0 +1,27 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+	
+	private String hobby;
+	
+	
+	public Human() {
+		
+	}
+	public Human(String name, int age, String hobby) {
+		super(name, age);
+		this.hobby = hobby;
+		
+		
+
+	}
+	public String getHobby() {
+		return hobby;
+	}
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	public void think() {
+		System.out.println("私は" + hobby + "について考えています");
+	}
+}
